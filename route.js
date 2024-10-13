396a4d986635b43b771d571cd181e27ffd8b8c5b
Harshit sharma <programming966051@gmail.com>
2024-10-13 12:04:47 +0530

    add routing

---
+++ route.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import ImagePage from '@/components/ImagePage.vue';
+
+
+const routes = [
+  {
+    name: 'ImagePage',
+    component: ImagePage,
+    path: '/Image'
+  }
+];
+
+
+const router = createRouter({
+  history: createWebHistory(), 
+  routes 
+});
+
+export default router;
