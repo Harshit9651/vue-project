dadafb2b05612e5ebe7754b7be479e4b87d2a259
Harshit sharma <programming966051@gmail.com>
2024-10-14 15:54:37 +0530

    making all cahnges

--- src/components/ImagePage.vue
+++ src/components/ImagePage.vue
@@ -16,10 +16,8 @@ let canvas;
 function initCanvas() {
   canvasElement = document.getElementById("canvas");
 
-  // Get the canvas-wrapper element
   const canvasWrapper = document.querySelector(".canvas-wrapper");
 
-  // Set up canvas width and height according to the wrapper
   const canvasWidth = canvasWrapper.clientWidth;
   const canvasHeight = canvasWrapper.clientHeight;
 
@@ -28,26 +26,21 @@ function initCanvas() {
   fabric.Image.fromURL(
     "https://images.pexels.com/photos/16465970/pexels-photo-16465970/free-photo-of-a-woman-posing-in-a-vast-yellow-flower-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     function (img) {
-      // Get the aspect ratio of the image
       const imgRatio = img.width / img.height;
       const wrapperRatio = canvasWidth / canvasHeight;
 
       let scaleX, scaleY, finalWidth, finalHeight;
 
-      // Determine whether to scale based on width or height
       if (imgRatio > wrapperRatio) {
-        // Image is wider than the wrapper, scale based on width
         scaleX = scaleY = canvasWidth / img.width;
         finalWidth = canvasWidth;
         finalHeight = img.height * scaleY;
       } else {
-        // Image is taller or proportional, scale based on height
         scaleX = scaleY = canvasHeight / img.height;
         finalWidth = img.width * scaleX;
         finalHeight = canvasHeight;
       }
 
-      // Set the canvas dimensions to match the scaled image
       canvas.setWidth(finalWidth);
       canvas.setHeight(finalHeight);
 
@@ -56,7 +49,6 @@ function initCanvas() {
         scaleY,
       });
 
-      // Set the image as the background
       canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
     }
   );
@@ -81,7 +73,7 @@ onMounted(() => {
     background-color: rgb(247, 106, 18);
   }
   canvas {
-    border: 1px solid #ccc;
+    // border: 1px solid #ccc;
     background-color: bisque;
   }
 }
