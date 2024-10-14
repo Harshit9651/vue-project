b093623e9f7dd3a37b757439bfdb4b3fce57572f
Harshit sharma <programming966051@gmail.com>
2024-10-14 15:29:54 +0530

    accouring error on imag set

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#app{
+  height: 100%;
+  width: 100%;
+}
+body{
+  height: 100%;
+  width: 100%;
+}
 
 </style>
--- src/components/ImagePage.vue
+++ src/components/ImagePage.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="page-wrapper">
     <div class="canvas-wrapper"><canvas id="canvas"></canvas></div>
@@ -20,6 +19,11 @@ function initCanvas() {
   fabric.Image.fromURL(
     "https://images.pexels.com/photos/16465970/pexels-photo-16465970/free-photo-of-a-woman-posing-in-a-vast-yellow-flower-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     function (img) {
+      img.set({
+        height: 500,
+        width: 500,
+        scaleX: 1,
+      });
       canvas.setWidth(img.width);
       canvas.setHeight(img.height);
 
@@ -38,16 +42,22 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .page-wrapper {
+  height: 100%;
+  width: 100%;
   .canvas-wrapper {
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 70vw;
+    height: 80vh;
     -webkit-box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
     -moz-box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
     box-shadow: 1px -4px 15px -1px rgba(110, 62, 110, 1);
+    background-color: rgb(247, 106, 18);
   }
   canvas {
-    border: 1px solid #ccc; 
+    border: 1px solid #ccc;
+    background-color: bisque;
   }
 }
 </style>
